8e5f94583548ac205d9fe8c59d86f2b1ab8f909e
truuck44 <guilhermetntt@gmail.com>
2020-05-30 14:44:37 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,26 @@
+/** Este programa realiza operações com filas e pilhas
+* Serão feitas representações em vetores e encadeadas
+* para cada uma das estruturas. Os dados armazenados  * em cada estrutura terá como informações o atendimento de um paciente.
+* 1) Operações Com Pilha Encadeada
+* a)Inserção de um atendimento na Pilha
+* b)Remoção de um atendimento na Pilha, mostrando os  * dados do elemento removido
+
+* 2) Operações Com Fila Encadeada
+* a)Inserção de um atendimento na Fila
+* b)Remoção de um atendimento na Fila, mostrando os   * dados do elemento removido
+
+* 3) Operações Com Pilha em Vetor
+* a)Inserção de um atendimento na Pilha
+* b)Remoção de um atendimento na Pilha, mostrando os   * dados do elemento removido
+
+* 4) Operações Com Fila em Vetor
+* a)Inserção de um atendimento na Fila
+* b)Remoção de um atendimento na Fila, mostrando os    * dados do elemento removido
+*/
+
+#include <stdio.h>
+
+int main(void) {
+  printf("Hello World\n");
+  return 0;
+}
